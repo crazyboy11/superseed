e611676a3573657654a69ea8137675535b3fded3
crazyboy11 <vito.vcp@outlook.com>
2016-03-16 15:41:53 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {172178919},--Sudo users
+    sudo_users = {172178919,24461551},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
